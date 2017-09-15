397483dfb93770e1473e3c416c4c0e0623c94740
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-15 09:52:17 -0700

    Made a change to be able to make a commit.

--- NestedForLoops/Program.cs
+++ NestedForLoops/Program.cs
@@ -5,7 +5,7 @@ namespace NestedForLoops
     {
         //this is the method that NestedForLoops will be executed
         //static void Main is taking 2 strings and a 2D array which is assigned
-        //to a new string with 5 rows and columns to create set up the nested array
+        //to a new string with 5 rows and columns to create set up the nested array.
         static void Main(string[] args)
         {
             string c = "catnip ";
